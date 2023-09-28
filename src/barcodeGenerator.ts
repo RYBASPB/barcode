15ec7a224dfec3afea99f586afbb1ead1195c548
Alexandr Demidov <alexander.demidov.1997@gmail.com>
2023-09-28 23:20:10 +0300

    landscape 60x58

--- src/barcodeGenerator.ts
+++ src/barcodeGenerator.ts
@@ -1,82 +1,121 @@
 import JsBarcode from "jsbarcode";
 import jsPDF from "jspdf";
 import html2canvas from "html2canvas";
-import {IBook} from "./main.ts";
+import { IBook } from "./main.ts";
 
-export function createBookCard(bookInfo : IBook) {
+export function createBookCard(bookInfo: IBook) {
+  const bookCard = document.createElement("div");
+  const printButton = document.createElement("button");
+  const bookContainer = document.createElement("div");
+  const barcodeContainer = document.createElement("div");
+  const barcodeElement = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "svg"
+  );
+  const sellerName = document.createElement("div");
+  sellerName.textContent = bookInfo.seller;
+  sellerName.className = "seller-name";
 
-    const bookCard = document.createElement("div");
-    const printButton = document.createElement("button");
-    const bookContainer = document.createElement("div");
-    const barcodeContainer = document.createElement("div");
-    const barcodeElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    bookCard.className = "book_card";
+  bookCard.className = "book_card";
 
-    printButton.className = "button-55";
-    printButton.innerText = "Печать";
-    bookContainer.className = "container";
-    barcodeContainer.className = "barcode";
-    barcodeElement.setAttribute("class", "barcode_image");
+  printButton.className = "button-55";
+  printButton.innerText = "Печать";
+  bookContainer.className = "container";
+  barcodeContainer.className = "barcode";
+  barcodeElement.setAttribute("class", "barcode_image");
 
-    bookContainer.id = bookInfo.barcode;
-    printButton.dataset.barcode = bookInfo.barcode;
+  bookContainer.id = bookInfo.barcode;
+  bookContainer.dataset.seller = bookInfo.seller;
+  bookContainer.dataset.name = bookInfo.name;
+  printButton.dataset.barcode = bookInfo.barcode;
 
-    printButton.addEventListener("click", e => {
-        e.preventDefault();
-        if (e.target instanceof HTMLButtonElement) {
-            const barcode = e.target.dataset.barcode;
-            barcode && createCanvas(barcode);
-        }
-    })
+  const textElement = createText(bookInfo);
+  bookCard.appendChild(sellerName);
+  bookCard.appendChild(bookContainer);
+  bookCard.appendChild(printButton);
+  bookContainer.appendChild(barcodeContainer);
+  barcodeContainer.appendChild(barcodeElement);
+  bookContainer.appendChild(textElement);
+  createBarcode(barcodeElement, bookInfo.barcode);
 
-
-    const bookCardsContainer = document.querySelector(".book_cards_container");
-    if (bookCardsContainer) {
-        const textElement = createText(bookInfo);
-        bookCardsContainer.appendChild(bookCard);
-        bookCard.appendChild(bookContainer);
-        bookCard.appendChild(printButton);
-        bookContainer.appendChild(barcodeContainer);
-        barcodeContainer.appendChild(barcodeElement);
-        bookContainer.appendChild(textElement);
-        createBarcode(barcodeElement, bookInfo.barcode);
+  printButton.addEventListener("click", e => {
+    e.preventDefault();
+    if (e.target instanceof HTMLButtonElement) {
+      const barcode = e.target.dataset.barcode;
+      barcode && createCanvas(barcode);
     }
+  });
+
+  return bookCard;
 }
-function createBarcode(element : SVGSVGElement ,number : string) {
-    JsBarcode(element, number, {
-        displayValue: false,
-        height: 60,
-        margin:0,
-    });
+function createBarcode(element: SVGSVGElement, number: string) {
+  JsBarcode(element, number, {
+    displayValue: false,
+    height: 60,
+    margin: 0
+  });
 }
 
-function createText(bookInfo : IBook) : HTMLDivElement {
-    const textElement = document.createElement("div");
-    textElement.className = "text";
-    textElement.innerHTML = `
+function createText(bookInfo: IBook): HTMLDivElement {
+  const textElement = document.createElement("div");
+  textElement.className = "text";
+  textElement.innerHTML = `
         <b>${bookInfo.barcode}</b>
         <p>${bookInfo.brand}</p>
         <p>${bookInfo.name}</p>
         <p>${bookInfo.author}</p>
         <p>Артикул: ${bookInfo.code}</p>
     `;
-    return textElement;
+  return textElement;
+}
+
+export function createLayout(groupedBooksInfo: Record<string, IBook[]>) {
+  const bookCardsContainer = document.querySelector(".book_cards_container");
+  if (bookCardsContainer) {
+    const groupsOfBooks = createGroupOfBookCards(groupedBooksInfo);
+    groupsOfBooks.forEach(group => {
+      bookCardsContainer.appendChild(group);
+    });
+  }
+}
+
+function createGroupOfBookCards(groupedBooksState: Record<string, IBook[]>) {
+  let bookGroups: HTMLDivElement[] = [];
+  for (const groupName in groupedBooksState) {
+    const group = document.createElement("div");
+    group.className = "group-of-cards";
+    const groupHeader = document.createElement("h2");
+    const cardsContainer = document.createElement("div");
+    groupHeader.className = "group-of-cards__header";
+    groupHeader.textContent = groupName;
+    cardsContainer.className = "group-of-cards__container";
+    group.appendChild(groupHeader);
+    group.appendChild(cardsContainer);
+    groupedBooksState[groupName].forEach(book => {
+      const bookElement = createBookCard(book);
+      cardsContainer.appendChild(bookElement);
+    });
+    bookGroups.push(group);
+  }
+  return bookGroups;
 }
 
 export async function createCanvas(barcode: string) {
-    const container = document.getElementById(barcode);
-    if (container) {
-        const canvas : HTMLCanvasElement = await html2canvas(container, {
-            scale: 5
-        });
-        const data = canvas.toDataURL('image/png');
-        const doc = new jsPDF({
-            orientation: "landscape",
-            unit: "mm",
-            format: [60,58]
-        });
-        doc.addImage(data, 'PNG', 60, -2, 58, 60, "", "NONE", 90);
-        doc.save(`${barcode}.pdf`)
-    }
-
+  const container = document.getElementById(barcode);
+  if (container) {
+    const prefix = container.dataset.seller;
+    const bookName = container.dataset.name;
+    const normalizedBookName = bookName!.replace(/[^a-zА-яа-я0-9]+/g, '');
+    const canvas: HTMLCanvasElement = await html2canvas(container, {
+      scale: 5
+    });
+    const data = canvas.toDataURL("image/png");
+    const doc = new jsPDF({
+      orientation: "landscape",
+      unit: "mm",
+      format: [60, 58]
+    });
+    doc.addImage(data, "PNG", 60, -2, 58, 60, "", "NONE", 90);
+    doc.save(`${prefix}${barcode.slice(-4)}${normalizedBookName}.pdf`);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,63 +1,38 @@
-import { createBookCard, createCanvas } from "./barcodeGenerator.ts";
+import { createCanvas, createLayout } from "./barcodeGenerator.ts";
 import { read, utils } from "xlsx";
 
 let booksState: IBook[] = [];
+let groupedBooksState: Record<string, IBook[]> = {};
 export interface IBook {
   barcode: string;
   brand: string;
   name: string;
   author: string;
   code: string;
+  seller: string;
 }
 
-function createBookCards(books: IBook[]) {
-  for (const b of books) {
-    createBookCard(b);
-  }
-}
-
-function onLoadFromLocalStorage() {
-  if (booksState.length) {
-    alert("Данные уже загружены");
-  } else {
-    const booksJSON = localStorage.getItem("books");
-    const books = JSON.parse(booksJSON!);
-
-    booksState = books;
-    createBookCards(books);
-  }
-}
-async function onSubmitCreateBarcodeListener(e: SubmitEvent) {
-  e.preventDefault();
-  if (booksState.length) {
-    booksState = [];
-    document.querySelector(".book_cards_container")!.innerHTML = "";
-  }
-  if (e.target && e.target instanceof HTMLFormElement) {
-    const elements = e.target.elements;
-    if (
+async function getBooksInfoFromXLSX(elements : HTMLFormControlsCollection) {
+  if (
       "goodsTable" in elements &&
       elements.goodsTable instanceof HTMLInputElement &&
       elements.goodsTable.files
-    ) {
-      const table = elements.goodsTable.files[0];
-      const jsonFromTable = await excelToJson(table);
-      if (jsonFromTable instanceof Array) {
-        const books = jsonFromTable.map(row => {
-          const bookParams: IBook = {
-            author: row["Автор"],
-            barcode: row["Баркод"],
-            brand: row["Бренд"],
-            code: row["Артикул продавца"],
-            name: row["Наименование"]
-          };
-          return bookParams;
-        });
-
-        booksState = books;
-        saveToLocalStorage(books);
-        createBookCards(books);
-      }
+  ) {
+    const table = elements.goodsTable.files[0];
+    const jsonFromTable = await excelToJson(table);
+    if (jsonFromTable instanceof Array) {
+      const books = jsonFromTable.map((row) => {
+        const bookParams: IBook = {
+          author: row["Автор"],
+          barcode: row["Баркод"],
+          brand: row["Бренд"],
+          code: row["Артикул продавца"],
+          name: row["Наименование"],
+          seller: row["seller"]
+        };
+        return bookParams;
+      });
+      return books;
     }
   }
 }
@@ -65,12 +40,72 @@ async function onSubmitCreateBarcodeListener(e: SubmitEvent) {
 async function excelToJson(table: File) {
   const data = await table.arrayBuffer();
   const workbook = read(data);
+  let jsonDataFromAllWorksheets: Record<string, string>[] = [];
   try {
-    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-    const jsonFromTable: object = utils.sheet_to_json(worksheet);
-    return jsonFromTable;
+    for (const worksheetName in workbook.Sheets) {
+      const worksheet = workbook.Sheets[worksheetName];
+      const jsonFromTable: Record<string, string>[] = utils.sheet_to_json(worksheet);
+      const jsonFromTableWithSeller: Record<string, string>[] = jsonFromTable.map((row : Record<string, string>) => {
+        row["seller"] = worksheetName;
+        return row;
+      })
+      jsonDataFromAllWorksheets = [...jsonDataFromAllWorksheets, ...jsonFromTableWithSeller];
+    }
+    return jsonDataFromAllWorksheets;
   } catch (e) {
     console.error(e);
+  }
+}
+
+function groupBooksBySeller(books : IBook[]) {
+  let bookGroups : Record<string, IBook[]> = {};
+  books.forEach(book => {
+    if (bookGroups[book.seller]) {
+      bookGroups[book.seller].push(book);
+    } else {
+      bookGroups[book.seller] = [book];
+    }
+  });
+  return bookGroups;
+}
+
+function createBookCards() {
+  createLayout(groupedBooksState);
+}
+function onLoadFromLocalStorage() {
+  if (booksState && booksState.length) {
+    alert("Данные уже загружены");
+  } else {
+    const booksJSON = localStorage.getItem("books");
+    const groupedBooksJSON = localStorage.getItem("groupedBooks");
+    if (booksJSON && groupedBooksJSON) {
+      const books = JSON.parse(booksJSON!);
+      const groupedBooks = JSON.parse(groupedBooksJSON!);
+      booksState = books;
+      groupedBooksState = groupedBooks;
+      createBookCards();
+    } else {
+      alert("Данные еще не загружены!")
+    }
+  }
+}
+
+async function onSubmitCreateBarcodeListener(e: SubmitEvent) {
+  e.preventDefault();
+  if (booksState && booksState.length) {
+    booksState = [];
+    document.querySelector(".book_cards_container")!.innerHTML = "";
+  }
+  if (e.target && e.target instanceof HTMLFormElement) {
+    const elements = e.target.elements;
+    const books = await getBooksInfoFromXLSX(elements);
+    if (books) {
+      const groupedBooks = groupBooksBySeller(books);
+      booksState = books;
+      groupedBooksState = groupedBooks;
+      saveToLocalStorage(books, groupedBooks);
+      createBookCards();
+    }
   }
 }
 
@@ -122,8 +157,9 @@ function changeFileNameOnLoadListener() {
   }
 }
 
-function saveToLocalStorage(books: IBook[]) {
+function saveToLocalStorage(books: IBook[], groupedBooks: Record<string, IBook[]>) {
   localStorage.setItem("books", JSON.stringify(books));
+  localStorage.setItem("groupedBooks", JSON.stringify(groupedBooks));
 }
 
 addBarcodeButtonListener();
